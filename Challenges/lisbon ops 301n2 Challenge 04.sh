b911556fcf25041f560b101c9912d6ee49a140a1
tomasferreira06 <tomas.ferreira206@gmail.com>
2024-03-28 10:22:55 +0000

    Changed names

---
+++ Challenges/lisbon ops 301n2 Challenge 04.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+clear
+while true; do 
+echo -e "\n"
+echo "1. Hello World!"
+echo "2. Ping Self."
+echo "3. IP Info."
+echo "4. Exit."
+ 
+read -p "Please choose an option:" option
+
+    if [ "$option" == "1" ]; then 
+        echo -e "\n"
+        echo "Hello World!"
+
+    elif [ "$option" == "2" ]; then
+        ping -c 4 localhost
+
+    elif [ "$option" == "3" ]; then
+        echo -e "\n"
+        ifconfig 
+
+    elif [ "$option" == "4" ]; then
+        echo "Exit Successful!"
+        exit 0
+
+    else 
+        echo "Invalid Input!"
+        exit 1
+    fi
+done
