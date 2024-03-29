352a446b81ba631dd88a50480b90f0e63f6dba4d
tomasferreira06 <tomas.ferreira206@gmail.com>
2024-03-29 20:02:10 +0000

    Added challenge08

---
+++ Challenges/lisbon ops 301n2 Challenge Class08.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+#Creating the variable "lista" with the 10 elements
+lista = ["a","b","c","d","e","f","g","h","i","j"]
+
+#Prints the fourth element of the list
+print("Fourth element of the list:" , lista[3])
+
+#Prints the sixth to tenth elements of the list
+print("Sixth through tenth elements of the list:" , lista[5:10])
+
+#Changes the seventh element of the variable "lista"
+lista[6]= "onion"
+
+#Prints the updated list
+print("Updated list:" , lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
