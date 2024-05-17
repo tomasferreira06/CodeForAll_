320270956120ab2948d96b0ec3e97b4c8160048f
tomasferreira06 <tomas.ferreira206@gmail.com>
2024-05-17 18:39:59 +0100

    script

--- Challenges/lisbon ops 401n2 Challenge Class 06.py
+++ Challenges/lisbon ops 401n2 Challenge Class 06.py
@@ -1,5 +1,23 @@
 #!/bin/python3
 
+# Import Libraries
+
+from cryptography.fernet import Fernet 
+
+# Define Functions  
+
+# This function generates a key and saves it into a file
+def write_key():
+    key = Fernet.generate_key()
+
+
+
+
+
+
+
+
+
 print("To encrypt a file, enter 1.")
 print("To decrypt a file, enter 2.")
 print("To encrypt a message, enter 3.")
@@ -7,14 +25,22 @@ print("To decrypt a message, enter 4.")
 
 mode = input("Enter your option: ")
 
-if mode == 1:
-    print("Provide path")
-elif mode == 2:
-    print("provide path")
-elif mode == 3:
-    print("provide cleartext")
-elif mode == 4:
-    print("provide ciphertext")
+if mode == "1":
+    print("Provide a path to the file: ")
+
+
+elif mode == "2":
+    print("Provide a path to the file: ")
+
+
+elif mode == "3":
+    print("Provide the cleartext string: ")
+
+
+elif mode == "4":
+    print("Provide the ciphertext string: ")
+
+
 else:
     print("Wrong input! Please try again.")
 
