7df3532e576f0ec40e0f9d317e7720256677f8e8
tomasferreira06 <tomas.ferreira206@gmail.com>
2024-05-19 20:13:24 +0100

    upload

--- Challenges/lisbon ops 401n2 Challenge Class 06.py
+++ Challenges/lisbon ops 401n2 Challenge Class 06.py
@@ -3,31 +3,50 @@
 # Import Libraries
 
 from cryptography.fernet import Fernet 
+import os
 
 # Define Functions  
 
 # This function generates a key and saves it into a file
 def write_key():
     key = Fernet.generate_key()
+    with open("key.key", "wb")as key_file:
+        key_file.write(key)
 
 
+def load_key():
+    return open("key.key", "rb").read()
 
+def encrypt_file(filepath, key):
 
+    fernet = Fernet(key)
 
+    with open(filepath, "rb") as file:
+        file_data = file.read()
 
+        encrypted_data = fernet.encrypt(file_data)
 
+        with open(filepath, "wb") as file:
+            file.write(encrypted_data)
 
+if not os.path.exists("key.key"):
+    write_key()
 
-print("To encrypt a file, enter 1.")
-print("To decrypt a file, enter 2.")
-print("To encrypt a message, enter 3.")
-print("To decrypt a message, enter 4.")
+    key = load_key()
 
-mode = input("Enter your option: ")
+# Print the menu
+
+mode = input("""To encrypt a file, enter 1.
+To decrypt a file, enter 2. 
+To encrypt a message, enter 3. 
+To decrypt a message, enter 4. 
+Enter your option: """)
 
 if mode == "1":
-    print("Provide a path to the file: ")
+    filepath = input("Provide a path to the file: ")
 
+    encrypt_file(filepath, key)
+    print(f"The file '{filepath}' has been encrypted and overwritten.")
 
 elif mode == "2":
     print("Provide a path to the file: ")
