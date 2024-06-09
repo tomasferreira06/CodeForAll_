40b49a67838af339f3125e5c455fb004e7b0dc1a
tomasferreira06 <tomas.ferreira206@gmail.com>
2024-06-09 18:17:31 +0100

    script challenge 16 part

--- Challenges/lisbon ops 401n2 Challenge Class 16.17.18.py
+++ Challenges/lisbon ops 401n2 Challenge Class 16.17.18.py
@@ -1,21 +1,49 @@
-from pwn import * 
-import paramiko
+import sys
+import time
 
-host = "127.0.0.1"
-username = "kali"
-attempts = 0
+def file_iteration(file):
 
-with open("100-password-list.txt", "r") as password_list:
-	for password in password_list:
-		password = password.strip("\n")
-		try:
-			print("[{}] Attempting password: '{}'!".format(attempts, password))
-			response = ssh(host=host, user=username, password=password, timeout=1)
-			if response.connected():
-				print("[>] Valid password found: '{}'!".format(password))
-				response.close()
-				break
-			response.close()
-		except paramiko.ssh_exception.AuthenticationException:
-			print("[X] Invalid password!")
-		attempts += 1
+	with open(file, "r") as password_list:
+		for password in password_list:
+			password = password.strip("\n")
+			print(password)
+			time.sleep(1)
+
+def string_search(string, file):
+
+	with open(file, "r") as password_list:
+		for password in password_list:
+			password = password.strip("\n")
+
+			if(string.lower() == password):
+
+				print("\nThe string you entered is in the provided password list!")
+				print("\n")
+				sys.exit(0)
+
+		print("\nThe string you entered is not in the provided pasword list!")
+		print("The program will no exit...")
+		sys.exit(0)
+
+mode = input("""To iterate through list enter 1.
+To input a string and compare with list enter 2.
+Choose your option: """)
+
+if mode == "1":
+    
+    file = input("Please enter the password list name (ex. password.txt): ")
+    
+    file_iteration(file)
+    
+elif mode == "2":
+    
+    string = input("Please provide the string used for the search: ")
+    file = input("Please enter the password list name (ex. password.txt): ")
+    
+    string_search(string, file)
+    
+else:
+    
+    print("Wrong input! Exiting program..")
+    sys.exit(1)
+
